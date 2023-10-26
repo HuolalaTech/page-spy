4b07b2b89d822d876a161eb8e6c4e853fd0df1c2
blucas.wu <15555602203@163.com>
2023-10-26 09:55:19 +0800

    bundle storage data on 'get' action

--- types/lib/storage.d.ts
+++ types/lib/storage.d.ts
@@ -1,18 +1,37 @@
 export type DataType = 'localStorage' | 'sessionStorage' | 'cookie';
 export type ActionType = 'clear' | 'remove' | 'get' | 'set';
 
-type RestCookieInfo = {
-  domain: null | string;
-  path: string;
-  partitioned: boolean;
-  secure: boolean;
-  sameSite: 'lax' | 'strict' | 'none';
-  expires: null | number;
-};
-interface DataItem extends Partial<RestCookieInfo> {
-  id: string;
+type Data = Pick<CookieStoreValue, 'name' | 'value'> &
+  Partial<Omit<CookieStoreValue, 'name' | 'value'>>;
+
+export interface GetTypeDataItem {
   type: DataType;
-  action: ActionType;
-  name?: string;
-  value?: string;
+  action: 'get';
+  data: Data[];
 }
+
+export interface SetTypeDataItem extends Data {
+  type: DataType;
+  action: 'set';
+}
+
+export interface RemoveTypeDataItem {
+  type: DataType;
+  action: 'remove';
+  name: string;
+}
+
+export interface ClearTypeDataItem {
+  type: DataType;
+  action: 'clear';
+}
+
+export type TypeDataItem =
+  | GetTypeDataItem
+  | SetTypeDataItem
+  | RemoveTypeDataItem
+  | ClearTypeDataItem;
+
+export type DataItem = TypeDataItem & {
+  id: string;
+};
