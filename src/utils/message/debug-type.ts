9606bdce769f8af137f2b99292dcc6fae391ed11
blucas.wu <15555602203@163.com>
2023-10-27 17:29:49 +0800

    add 'database-pagination' interactive event and add 'basic' type data

--- src/utils/message/debug-type.ts
+++ src/utils/message/debug-type.ts
@@ -7,6 +7,7 @@ export const SYSTEM = 'system';
 export const NETWORK = 'network';
 export const PAGE = 'page';
 export const STORAGE = 'storage';
+export const DATABASE = 'database';
 /**
  * Interactive: some type which sended by developer and need to reply something
  */
@@ -15,3 +16,4 @@ export const REFRESH = 'refresh';
 export const ATOM_DETAIL = 'atom-detail';
 export const ATOM_GETTER = 'atom-getter';
 export const DEBUGGER_ONLINE = 'debugger-online';
+export const DATABASE_PAGINATION = 'database-pagination';
--- types/lib/database.d.ts
+++ types/lib/database.d.ts
@@ -1,13 +1,61 @@
-export interface DBStoreInfo<T = any> {
+export interface DBStoreInfo {
   name: string;
   keyPath: string | string[] | null;
   autoIncrement: boolean;
   indexes: string[];
-  data: T;
 }
 
 export interface DBInfo {
-  database: string;
-  version?: number;
+  name: string;
+  version: number;
   stores: DBStoreInfo[];
 }
+
+interface BasicTypeDataItem {
+  action: 'basic';
+  result: DBInfo[] | null;
+}
+
+interface GetTypeDataItem<T = any> {
+  action: 'get';
+  database: Omit<DBInfo, 'stores'> | null;
+  store: DBStoreInfo | null;
+  page: {
+    prev: number | null;
+    current: number;
+    next: number | null;
+  };
+  total: number;
+  data: T;
+}
+
+interface DropTypeDataItem {
+  // indexedDB.deleteDatabase()
+  action: 'drop';
+  database: string;
+}
+
+interface UpdateTypeDataItem {
+  // store.put() || store.add() || store.delete()
+  action: 'update';
+  database: string;
+  store: string;
+}
+
+interface ClearTypeDataItem {
+  // store.clear()
+  action: 'clear';
+  database: string;
+  store: string;
+}
+
+type TypeDataItem =
+  | BasicTypeDataItem
+  | GetTypeDataItem
+  | DropTypeDataItem
+  | UpdateTypeDataItem
+  | ClearTypeDataItem;
+
+export type DataItem = TypeDataItem & {
+  id: string;
+};
