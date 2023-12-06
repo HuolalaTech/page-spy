6b9bf59294cd22fc7991dee1f771570a9fc294bf
blucas.wu <15555602203@163.com>
2023-12-06 11:07:13 +0800

    update storage unit tests

--- tests/plugins/storage.test.ts
+++ tests/plugins/storage.test.ts
@@ -9,7 +9,7 @@ beforeEach(() => {
   document.cookie = '';
 });
 afterEach(() => {
-  jest.restoreAllMocks();
+  jest.resetAllMocks();
 });
 
 describe('Storage plugin', () => {
@@ -18,17 +18,54 @@ describe('Storage plugin', () => {
     await cookieStore.set('1', '1');
     await cookieStore.set('2', '2');
     expect(trigger).toHaveBeenCalledTimes(2);
+    expect(trigger).lastCalledWith(
+      expect.objectContaining({
+        type: 'cookie',
+        action: 'set',
+      }),
+    );
     await cookieStore.delete('1');
     await cookieStore.delete('2');
     expect(trigger).toHaveBeenCalledTimes(4);
+    expect(trigger).lastCalledWith(
+      expect.objectContaining({
+        type: 'cookie',
+        action: 'remove',
+      }),
+    );
     localStorage.setItem('1', '1');
-    sessionStorage.setItem('2', '2');
-    expect(trigger).toHaveBeenCalledTimes(6);
     localStorage.removeItem('1');
+    expect(trigger).toHaveBeenCalledTimes(6);
+    expect(trigger).lastCalledWith(
+      expect.objectContaining({
+        type: 'localStorage',
+        action: 'remove',
+      }),
+    );
+    sessionStorage.setItem('2', '2');
     sessionStorage.removeItem('2');
     expect(trigger).toHaveBeenCalledTimes(8);
+    expect(trigger).lastCalledWith(
+      expect.objectContaining({
+        type: 'sessionStorage',
+        action: 'remove',
+      }),
+    );
     localStorage.clear();
+    expect(trigger).lastCalledWith(
+      expect.objectContaining({
+        type: 'localStorage',
+        action: 'clear',
+      }),
+    );
+    expect(trigger).toHaveBeenCalledTimes(9);
     sessionStorage.clear();
+    expect(trigger).lastCalledWith(
+      expect.objectContaining({
+        type: 'sessionStorage',
+        action: 'clear',
+      }),
+    );
     expect(trigger).toHaveBeenCalledTimes(10);
   });
 });
