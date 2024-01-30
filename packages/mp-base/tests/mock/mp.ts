345213129dd087e8888225348e7a26bc0c7c878c
qikang.yuan <me@qkang.top>
2024-01-30 15:46:49 +0800

    update test code for single socket

--- packages/mp-base/tests/mock/mp.ts
+++ packages/mp-base/tests/mock/mp.ts
@@ -1,10 +1,38 @@
 import { SocketState } from 'base/src/socket-base';
 import { mockRequest } from './request';
+import EventEmitter from 'events';
 type CBType = (event?: any) => any;
+
+export class MockSocket {
+  private ee = new EventEmitter();
+  status: SocketState = SocketState.CONNECTING;
+  send(params: { data: string | Buffer }) {}
+  onOpen(handler: (res: any) => void) {
+    this.status = SocketState.OPEN;
+    this.ee.addListener('open', handler);
+  }
+  onClose(handler: (res: any) => void) {
+    this.ee.addListener('close', handler);
+  }
+  onError(handler: (msg: string) => void) {
+    this.ee.addListener('error', handler);
+  }
+  onMessage(handler: (data: string | Buffer) => void) {
+    this.ee.addListener('message', handler);
+  }
+  close() {
+    if (this.status !== SocketState.CLOSED) {
+      return;
+    }
+    this.status = SocketState.CLOSED;
+    this.ee.emit('close');
+  }
+}
 
 export class MockMP implements MPSystemAPI, MPNetworkAPI, MPStorageAPI {
   private store: Record<string, any> = {};
   private listeners = new Map<string, CBType[]>();
+  private socketInstance: MockSocket | null = null;
   constructor() {
     this.listeners.set('onError', []);
     this.listeners.set('onUnHandledRejection', []);
@@ -92,36 +120,59 @@ export class MockMP implements MPSystemAPI, MPNetworkAPI, MPStorageAPI {
   request = mockRequest;
 
   connectSocket(params: { url: string }) {
-    let closeHandler: (res: any) => void;
-    let openHandler: (res: any) => void;
-    let messageHandler: (data: object) => void;
-    let errorHandler: (msg: string) => void;
-    let status: SocketState = SocketState.OPEN;
-    return {
-      send(data: object) {},
-      onOpen(handler: (res: any) => void) {
-        status = SocketState.OPEN;
-        openHandler = handler;
-      },
-      onClose(handler: (res: any) => void) {
-        closeHandler = handler;
-      },
-      onError(handler: (msg: string) => void) {
-        errorHandler = handler;
-      },
-      close() {
-        if (status !== SocketState.CLOSED) {
-          return;
-        }
-        status = SocketState.CLOSED;
-        if (closeHandler) {
-          closeHandler({});
-        }
-      },
-      onMessage(handler: (data: object) => void) {
-        messageHandler = handler;
-      },
-    } as MPSocket;
+    // let closeHandler: (res: any) => void;
+    // let openHandler: (res: any) => void;
+    // let messageHandler: (data: object) => void;
+    // let errorHandler: (msg: string) => void;
+    // let status: SocketState = SocketState.OPEN;
+    this.socketInstance = new MockSocket();
+    return this.socketInstance;
+    // return {
+    //   send(data: object) {},
+    //   onOpen(handler: (res: any) => void) {
+    //     status = SocketState.OPEN;
+    //     openHandler = handler;
+    //   },
+    //   onClose(handler: (res: any) => void) {
+    //     closeHandler = handler;
+    //   },
+    //   onError(handler: (msg: string) => void) {
+    //     errorHandler = handler;
+    //   },
+    //   close() {
+    //     if (status !== SocketState.CLOSED) {
+    //       return;
+    //     }
+    //     status = SocketState.CLOSED;
+    //     if (closeHandler) {
+    //       closeHandler({});
+    //     }
+    //   },
+    //   onMessage(handler: (data: object) => void) {
+    //     messageHandler = handler;
+    //   },
+    // } as MPSocket;
+  }
+
+  onSocketClose(handler: SocketOnCloseHandler): void {
+    this.socketInstance?.onClose(handler);
+  }
+  onSocketError(handler: SocketOnErrorHandler): void {
+    this.socketInstance?.onError(handler);
+  }
+  onSocketMessage(handler: SocketOnMessageHandler): void {
+    this.socketInstance?.onMessage(handler);
+  }
+  onSocketOpen(handler: SocketOnOpenHandler): void {
+    this.socketInstance?.onOpen(handler);
+  }
+  sendSocketMessage(
+    params: { data: string | Buffer } & AsyncCallback<any, any>,
+  ): void {
+    this.socketInstance?.send(params);
+  }
+  closeSocket(params: AsyncCallback<any, any>): void {
+    this.socketInstance?.close();
   }
 
   canIUse(api: string) {
