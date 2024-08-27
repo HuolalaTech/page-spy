d3c96065e9cd122107fbea5fdb327dd389d20609
blucas.wu <15555602203@163.com>
2024-08-27 16:00:52 +0800

    [harmony] fix: reset the DEVICE_INFO

--- packages/page-spy-harmony/library/src/main/ets/utils/constants.ts
+++ packages/page-spy-harmony/library/src/main/ets/utils/constants.ts
@@ -6,11 +6,12 @@ export const ROOM_SESSION_KEY = 'page-spy-room';
  * The combined device info.
  * See the screenshot: /library/src/main/resources/base/media/mate60_device_info.png
  *
- * Format: HarmonyOS/<deviceInfo.osFullName> Device/<deviceInfo.marketName.replace(/\s/g, '_')>/<deviceInfo.sdkApiVersion>
- *    e.g. "HarmonyOS/3.0.0.19(Canary2) Device/HUAWEI_Mate_60_Pro/API_12"
+ * Format: (<deviceInfo.deviceType>; OpenHarmony <deviceInfo.osFullName>) Device/<deviceInfo.marketName.replace(/\s/g, '_')> Api/<deviceInfo.sdkApiVersion>
+ *    e.g. "(Phone; OpenHarmony 3.0.0.19) Device/HUAWEI_Mate_60_Pro API/12"
  */
-export const DEVICE_INFO = `HarmonyOS/${
-  deviceInfo.osFullName
-} Device/${deviceInfo.marketName.replace(/\s/g, '_')}/API_${
-  deviceInfo.sdkApiVersion
-}`;
+
+const { deviceType, osFullName, marketName, sdkApiVersion } = deviceInfo;
+const formatDeviceType = deviceType[0].toUpperCase() + deviceType.slice(1);
+const formatMarketName = marketName.replace(/\s/g, '_');
+
+export const DEVICE_INFO = `(${formatDeviceType}; OpenHarmony ${osFullName}) Device/${formatMarketName} API/${sdkApiVersion}`;
