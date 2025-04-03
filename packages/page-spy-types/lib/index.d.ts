5d27e16ee700a7e6803f0b42868733942e12cc8f
blucas.wu <15555602203@163.com>
2025-04-03 17:52:40 +0800

    remove InitConfigBase from page-spy-types

--- packages/page-spy-types/lib/index.d.ts
+++ packages/page-spy-types/lib/index.d.ts
@@ -1,99 +1,10 @@
 import { SocketStoreBase } from '@huolala-tech/page-spy-base';
-import { DataItem as ConsoleData } from './console';
-import { DataItem as StorageData } from './storage';
-import { DataItem as PageData } from './page';
-import { DataItem as DatabaseData } from './database';
-import { DataItem as SystemData } from './system';
-import { RequestInfo } from './network';
 import { Modal } from './modal';
 import { Toast } from './toast';
-
-export interface InitConfigBase {
-  /**
-   * The server base url. For example, "example.com".
-   * - Create room: `https://${api}/room/create`
-   * - Filter room: `https://${api}/room/list`
-   * - Join WebSocket room: `wss://${api}/ws/room/join`
-   */
-  api?: string;
-
-  /**
-   * Project name, used for group connections
-   */
-  project?: string;
-
-  /**
-   * Custom title for displaying some data like user info to
-   * help you to distinguish the client. The title value will
-   * show in the room-list route page.
-   */
-  title?: string;
-
-  /**
-   * Specify the server <scheme> manually.
-   * - false: sdk will use ['http://', 'ws://'];
-   * - true: sdk will use ['https://', 'wss://'];
-   * - null: sdk will automatically analyse the scheme by the
-   *        'src' property value
-   */
-  enableSSL?: boolean | null;
-
-  /**
-   * Specify how many messages to cache.
-   * The data is primarily used for define "socketStore.messageCapacity" to
-   * configure the maximum number of historical data the SDK can send
-   * after the debugging terminal goes online.
-   */
-  messageCapacity?: number;
-
-  /**
-   * Indicate whether authorization is required. If enabled, PageSpy generates
-   * a 6-digit random number (below "secret") as a password for the debug room,
-   * which is required for developers to access the debug room
-   * @default false
-   */
-  useSecret?: boolean;
-
-  /**
-   * The 6-digit random number for authorization.
-   * @private
-   */
-  secret?: string;
-
-  /**
-   * Indicate whether serialize non-primitive data in offline log.
-   */
-  serializeData?: boolean;
-
-  /**
-   *
-   */
-  dataProcessor?: {
-    console?: (data: ConsoleData) => boolean;
-    network?: (data: RequestInfo) => boolean;
-    storage?: (data: StorageData) => boolean;
-    database?: (data: DatabaseData) => boolean;
-    page?: (data: PageData) => boolean;
-    system?: (data: SystemData) => boolean;
-  };
-
-  [key: string]: any;
-}
 
 export interface UpdateConfig {
   title?: string;
   project?: string;
-}
-
-export interface PageSpyBase {
-  version: string;
-  plugins: Record<string, PageSpyPlugin>;
-  config: Required<InitConfigBase> | null;
-  name: string;
-  address: string;
-  roomUrl: string;
-  abort(): void;
-  updateRoomInfo(obj: UpdateConfig): void;
 }
 
 interface CommonParams {
